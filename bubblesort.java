8d775e187d4d1a2a64331c0bcbca9a5f33912346
Parth-02-2005 <gandhiparth.02.2005@gmail.com>
2024-04-08 20:14:37 +0530

    all sorts
    all sorts

--- bubblesort.java
+++ bubblesort.java
@@ -1,4 +1,4 @@
-public class sort {
+public class bubblesort {
     public static void display(int arr[]){
         for(int i = 0; i<arr.length; i++){
             System.out.println(arr[i]);
---
+++ selectionsort.java
@@ -0,0 +1,24 @@
+public class selectionsort {
+    public static void display(int arr[]){
+        for(int i = 0; i<arr.length; i++){
+            System.out.println(arr[i]);
+        }
+        System.out.println();
+    }
+    public static void main(String[] args) {
+        int arr[] = {9,4,2,7,2};
+
+            for(int i = 0; i<arr.length-1; i++){
+                int smallest = i; 
+                for(int j = i+1; j<arr.length; j++){
+                    if (arr[smallest] > j) {
+                        smallest = j;
+                    }
+                }
+                int temp = arr[smallest];
+                arr[smallest] = arr[i];
+                arr[i] = temp;
+            }
+            display(arr);
+    }
+}
